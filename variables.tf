cfbb60fa9e09ec72a0f1fd923b25a648401831eb
Oleh Mykolaishyn <owlleg6@gmail.com>
2023-08-21 11:35:24 +0300

    fix: removed validation

--- variables.tf
+++ variables.tf
@@ -26,8 +26,4 @@ variable "nat_confiuration" {
     }))
     nat_ip_allocate_option = string
   }))
-  #   validation {
-  #     condition     = can(regex("^(ALL_SUBNETWORKS_ALL_IP_RANGES|ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES|LIST_OF_SUBNETWORKS)$", var.nat_confiuration[*].source_subnetwork_ip_ranges_to_nat)) && can(regex("^(MANUAL_ONLY|AUTO_ONLY)$", var.nat_confiuration[*].nat_ip_allocate_option))
-  #     error_message = "Invalid values. source_subnetwork_ip_ranges_to_nat should be one of: ALL_SUBNETWORKS_ALL_IP_RANGES, ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES, or LIST_OF_SUBNETWORKS. nat_ip_allocate_option should be one of: MANUAL_ONLY or AUTO_ONLY."
-  #   }
 }
